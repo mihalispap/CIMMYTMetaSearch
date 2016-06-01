3eb0ff4349af20bc5b00be4cc8e88a87a6f5a030
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-06-01 16:52:49 +0300

    msmanager.sh

---
+++ index/index_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for f in json/*.json;
+#for f in beta/*.json;
+do
+	sed -i 's/"date": 2\(.*\),/"date": "2\1",/g' $f
+	sed -i 's/"date": 1\(.*\),/"date": "1\1",/g' $f
+	./index_json.sh $f
+done
---
+++ msmanager.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+timestamp=$(date +%s)
+
+cd config/
+filename="oai.targets"
+
+echo "Starting harvesting process"
+echo "---------------------------"
+
+while read -r line
+do
+
+	url_name=$(echo ${line} | tr "\t" "\n")
+	IFS=', ' read -r -a arr <<< "${url_name}"
+
+	cd ../data
+	mkdir ${arr[1]}
+
+	cd ../harvester/bin
+	java -classpath ".:../lib/*" -Xss64M -Xmx2048M gr.agroknow.cimmyt.HarvestManager ${arr[0]} ../../data/${arr[1]} "oai_dc"
+
+	#echo ${arr[0]}
+	cd ../../config
+done < "$filename"
+
+echo "---------------------------"
+echo "Harvesting process complete"
+
+echo "Initializing internal transformation process"
+cd ../data
+
+for d in */ ; do
+	mv ${d}/*.xml ../internaltransformer/input
+	cd ../internaltransformer
+	java -jar saxon9he.jar -xsl:cimmyt.xsl -s:input -o:output
+	mv input/*.xml ../backup
+	cd ../data
+done
+echo "Transformation complete"
+
+cd ../
+mv internaltransformer/output/*.xml toenrich
+
+echo "Starting enrichment process"
+echo "---------------------------"
+
+cd enricher/bin
+java -classpath ".:../lib/*" -Xss64M -Xmx2048M com.agroknow.cimmyt.Enrich ../../toenrich ../../enriched
+
+echo "Enrichment process complete"
+
+
+
+
+
+
+
+
+
+
+
+current_timestamp=$(date +%s)
+let "diff_time=${current_timestamp}-${timestamp}"
+echo "Total time elapsed: "${diff_time}"secs"
