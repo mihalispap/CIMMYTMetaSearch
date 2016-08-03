8c5aaf9df1108fcb4fa3c4657c1addfcedb4c838
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-08-03 18:40:26 +0300

    additions

--- install_bonusl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo 'deb http://packages.elasticsearch.org/logstash/1.5/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
-sudo apt-get update
-sudo apt-get install logstash
